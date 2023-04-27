cb6e22e4da613465074f197d231ce253755fae93
J.M <kabugijeff@gmail.com>
2023-04-27 16:35:31 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import logo from '../logo.svg';
+import '../App.css';
+import Table from "./Table"
+
+function App() {
+  return (
+    <div className="App">
+      <h3 className="title">The Royal Bank of FlatIron</h3>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Table.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Table(){
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Description</th>
+                        <th>Category</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Table
